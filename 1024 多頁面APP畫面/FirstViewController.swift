a542d6187afb773bbb934fa2f8862d831bb5cf99
泳逸 李 <yongyili@yongyide-MacBook-Air.local>
2017-10-24 21:19:58 +0800

    Initial Commit

---
+++ 1024 多頁面APP畫面/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  1024 多頁面APP畫面
+//
+//  Created by 泳逸 李 on 2017/10/24.
+//  Copyright © 2017年 yungyili. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
